e8446f29959812f2f432b76e7b4e598461b0a4f4
Omar Usama <82762206+omarusama8882@users.noreply.github.com>
2024-10-03 10:53:24 +0200

    BN Channel initial testing done

--- RTL Inference Model/CNN/CNN.srcs/sources_1/new/BN_Channel.v
+++ RTL Inference Model/CNN/CNN.srcs/sources_1/new/BN_Channel.v
@@ -1,58 +1,40 @@
 module BN_Channel(
 clk,
-gamma,
-variance,
-average,
-beta,
+gvc,
+gmbvc,
 map,
 outmap
 );
     parameter H=5;
     parameter W=5;
     //localparam eps=16'b0000000000010000;
-    reg signed [1:0] sqrtpow=4'b00001000;
     input clk;
-    input signed [15:0] gamma;
-    input signed [15:0] variance;
-    input signed [15:0] average;
-    input signed [15:0] beta;
+    input signed [15:0] gvc;
+    input signed [15:0] gmbvc;
     input signed [(H*W*16)-1:0] map;
     output reg signed [(H*W*16)-1:0] outmap;
-    reg signed [15:0] addvarc;
-    reg  signed [15:0] sqrtvarc;
-    reg signed [31:0] gvcmult;
-    reg signed [15:0] gvc; 
-    reg signed [15:0]  varc;
-    reg signed [31:0] gmbvcmult;
-    reg signed [15:0] gm;
-    reg signed [15:0] gmbvc;
     reg signed [31:0] out1;
     reg signed [15:0] outsimplified;
     reg signed [15:0] newval;
+    //wire signed [15:0] gmbvcomplement;
     integer i;
     integer rowcounter;
     initial begin
     rowcounter=0;
+    
     end
-    always @(*) begin
-    //assign addvarc=eps+variance;
-     sqrtvarc=(variance**sqrtpow);
-     varc=1/sqrtvarc; 
-     gvcmult=gamma*varc;
-     gvc=gvcmult[31:16];
-     gmbvcmult=average*varc;
-     gm=gmbvcmult[27:12];
-     gmbvc=gm-beta;
-    end
+    //assign gmbvcomplement=~(gmbvc)+1'b1;
+    
+   
 always @(posedge clk) begin
         for(i=0;i<W;i=i+1) begin
             out1=map[16*(i+rowcounter)+:16]*gvc;        
-            outsimplified=out1[27:12];
-            newval=outsimplified-gmbvc;
+            outsimplified=out1[24:9];
+            newval=outsimplified+gmbvc;
             outmap[16*(i+rowcounter)+:16]=newval;
         end
-        if(rowcounter<(H*W*16)-1) begin
-        rowcounter=rowcounter+(W*16);
+        if(rowcounter<(H*W)) begin
+        rowcounter<=rowcounter+W;
         end
        
 end     
